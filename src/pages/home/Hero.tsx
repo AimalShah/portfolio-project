5c75378e8f063a4bca04d0e67c537e6c2fc677dc
aimalshah <aimalshah62@gamil.com>
2024-06-16 12:55:51 +0500

    add hero section

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,13 +1,15 @@
 
 export default function Hero() {
   return (
-    <div className=" container h-screen flex gap-2">
-        <div>
+    <div className=" container h-screen flex gap-2 mx-auto bg-red-600">
+        <div className="mt-40 ">
           <div>
-            <img src="https://reactfolio.tharindu.dev/logo.png" alt="" />
+            <img src="https://reactfolio.tharindu.dev/logo.png" alt="" 
+            className="w-20 rounded-full"
+            />
           </div>
           <div>
-            
+
           </div>
           <div>
 
