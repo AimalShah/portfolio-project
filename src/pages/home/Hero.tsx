b706e59f24ac936db3a7c128cdf7d16a7beee1a6
aimalshah <aimalshah62@gamil.com>
2024-06-20 15:09:40 +0500

    fixed

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faTwitterSquare , faInstagram , faWhatsapp , faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faFacebook , faInstagram , faWhatsapp , faLinkedin} from '@fortawesome/free-brands-svg-icons'
 
 export default function Hero() {
   return (
